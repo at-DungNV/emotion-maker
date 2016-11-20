82399863502cd448eb6a501ab1e24e7dea065c55
dungnv <nobinguyen94@gmail.com>
2016-11-20 20:57:05 +0700

    update

--- src/routes.php
+++ src/routes.php
@@ -2,23 +2,23 @@
 
 
 Route::get('emotion/index', [
-    'as' => 'emotion.index', 'uses' => 'php2\emotion-maker\EmotionController@index'
+    'as' => 'emotion.index', 'uses' => 'PHP2\EmotionMaker\EmotionController@index'
 ]);
 
 Route::get('emotion/index1', [
-    'as' => 'emotion.index1', 'uses' => 'php2\emotion-maker\EmotionController@index1'
+    'as' => 'emotion.index1', 'uses' => 'PHP2\EmotionMaker\EmotionController@index1'
 ]);
 
 
 Route::post('emotion/submit', [
-    'as' => 'emotion.submit', 'uses' => 'php2\emotion-maker\EmotionController@submit'
+    'as' => 'emotion.submit', 'uses' => 'PHP2\EmotionMaker\EmotionController@submit'
 ]);
 
 Route::get('emotion/show/{emotion?}', [
-    'as' => 'emotion.show', 'uses' => 'php2\emotion-maker\EmotionController@show'
+    'as' => 'emotion.show', 'uses' => 'PHP2\EmotionMaker\EmotionController@show'
 ]);
 
 
 Route::get('emotion/emotion', [
-    'as' => 'emotion.emotion', 'uses' => 'php2\emotion-maker\EmotionController@emotion'
+    'as' => 'emotion.emotion', 'uses' => 'PHP2\EmotionMaker\EmotionController@emotion'
 ]);
